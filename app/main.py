0855d4029473511a5f3a095ebf2e265530efc3a7
gsabatino <gsabatino@fi.uba.ar>
2022-09-08 19:18:01 -0300

    cambio cors

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:3000",
-    "https://fiuber-gateway.herokuapp.com/",
+    "https://taller-2-tyrions.github.io",
 ]
 
 app.add_middleware(
