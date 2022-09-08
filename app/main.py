bd544fbbcc2386adc1718391c798dface0555ef2
gsabatino <gsabatino@fi.uba.ar>
2022-09-08 13:46:15 -0300

    interaccion

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+import http3
 
 app = FastAPI()
+client = http3.AsyncClient()
+
+async def call_api(url: str):
+    r = await client.get(url)
+    return r.text
 
 
 @app.get("/")
-def root():
-    return {"Hello": "World"}
+async def root():
+    result = await call_api('https://fiuber-users.herokuapp.com/')
+    return {"Msg from users": result}
