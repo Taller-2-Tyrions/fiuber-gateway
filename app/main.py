2e150aa138b0b9e6baef8aa8595cddb25d38a91e
gsabatino <gsabatino@fi.uba.ar>
2022-09-08 17:03:18 -0300

    cambio cors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import http3
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 client = http3.AsyncClient()
@@ -8,6 +9,21 @@ client = http3.AsyncClient()
 async def call_api(url: str):
     r = await client.get(url)
     return r.text
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
