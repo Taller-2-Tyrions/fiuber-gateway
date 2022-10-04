44ca1ca036d44064b08d668b09e431e9340b23ce
mateocapon <mcapon@fi.uba.ar>
2022-10-04 10:16:11 -0300

    adding login with google

--- app/routers/login.py
+++ app/routers/login.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
 from ..schemas.users_schema import AuthBase
+from fastapi import Cookie
+from typing import Union
 from fastapi.encoders import jsonable_encoder
 import requests
 from dotenv import load_dotenv
@@ -19,6 +21,20 @@ router = APIRouter(
 @router.post('/')
 async def login(params: AuthBase):
     req = requests.post(USERS_URL+"/login", json=jsonable_encoder(params))
+
+    data = req.json()
+    if (req.status_code != 200):
+        raise HTTPException(detail=data["detail"], status_code=req.status_code)
+    return data
+
+
+@router.post('/google')
+async def login_google(token: Union[str, None] = Cookie(None)):
+    params = {
+        "token": token
+    }
+    req = requests.post(USERS_URL+"/login/google",
+                        json=jsonable_encoder(params))
 
     data = req.json()
     if (req.status_code != 200):
