1f169d8a411aac5a1765d37050ef5b207ab05e1f
Lucas Veron <veron_lucas@hotmail.com>
2022-10-10 22:49:56 -0300

    more changes in Gate voyage req

--- app/routers/voyage.py
+++ app/routers/voyage.py
@@ -27,28 +27,14 @@ def is_status_correct(status_code):
 @router.post('/user')
 async def init_voyage(voyage: SearchVoyageBase, token: TokenBase):
     uid = validate_request_and_get_uid(token)
-    # req = requests.post(USERS_URL+"/validate", json=jsonable_encoder(token))
-    # if (is_status_correct(req.status_code)):
-    #     resp = req.json()
-    #     if (resp["is_blocked"] or "User" not in resp["roles"]):
-    #         raise HTTPException(detail={
-    #             'message': 'Error Permission Denied',
-    #             'is_blocked': resp["is_blocked"],
-    #             'roles': resp["roles"]
-    #         }, status_code=400)
+
     voyage_body = jsonable_encoder(voyage)
     
     voyage_body["passenger"]["id"] = uid
-    
-    print("uid: "+uid+",voyage_body: "+str(voyage_body))
 
-    req = requests.post(VOYAGE_URL+"/voyage/user", json=voyage_body)
-    print(req)
-    if (not is_status_correct(req.status_code)):
-        data = req.json()
+    resp = requests.post(VOYAGE_URL+"/voyage/user", json=voyage_body)
+    data = resp.json()
+    if (not is_status_correct(resp.status_code)):
         raise HTTPException(detail=data["detail"],
-                            status_code=req.status_code)
-    return req.json()
-    # else:
-    #     raise HTTPException(detail=req.json()["detail"],
-    #                         status_code=req.status_code)
+                            status_code=resp.status_code)
+    return data
--- app/services/validation_services.py
+++ app/services/validation_services.py
@@ -11,13 +11,19 @@ def is_status_correct(status_code):
 
 
 def validate_request_and_get_uid(_token):
-    req = requests.post(USERS_URL+"/validate", json=jsonable_encoder(_token))
-    if (is_status_correct(req.status_code)):
-        resp = req.json()
+    resp0 = requests.post(USERS_URL+"/validate", json=jsonable_encoder(_token))
+    print("resp0: "+str(resp0))
+    if (is_status_correct(resp0.status_code)):
+        resp = resp0.json()
+        print("resp: "+resp)
         if (resp["is_blocked"] or "User" not in resp["roles"]):
             raise HTTPException(detail={
                 'message': 'Error Permission Denied',
                 'is_blocked': resp["is_blocked"],
                 'roles': resp["roles"]
             }, status_code=400)
-    return req.json()["uid"]
+    else:
+        raise HTTPException(detail={
+                'message': resp0.reason
+            }, status_code=resp0.status_code)
+    return resp["uid"]
