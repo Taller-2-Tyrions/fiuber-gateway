5098acc3b337efb8c6297b9ad1b99ccea07f2fc5
mateocapon <mcapon@fi.uba.ar>
2022-10-11 17:49:59 -0300

    gateway voyage

--- app/services/validation_services.py
+++ app/services/validation_services.py
@@ -10,13 +10,13 @@ def is_status_correct(status_code):
     return status_code//100 == 2
 
 
-def validate_request_and_get_uid(_token):
-    resp0 = requests.post(USERS_URL+"/validate", json=jsonable_encoder(_token))
+def validate_req_and_get_uid(token, role):
+    resp0 = requests.post(USERS_URL+"/validate", json=jsonable_encoder(token))
     print("resp0: "+str(resp0))
     if (is_status_correct(resp0.status_code)):
         resp = resp0.json()
         print("resp: "+resp)
-        if (resp["is_blocked"] or "User" not in resp["roles"]):
+        if (resp["is_blocked"] or role not in resp["roles"]):
             raise HTTPException(detail={
                 'message': 'Error Permission Denied',
                 'is_blocked': resp["is_blocked"],
@@ -27,3 +27,11 @@ def validate_request_and_get_uid(_token):
                 'message': resp0.reason
             }, status_code=resp0.status_code)
     return resp["uid"]
+
+
+def validate_req_driver_and_get_uid(token):
+    validate_req_and_get_uid(token, "Driver")
+
+
+def validate_req_user_and_get_uid(token):
+    validate_req_and_get_uid(token, "User")
